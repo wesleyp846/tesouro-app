248fee7180cf4bee3dadc90133782eee25275cfd
wesleyp846 <aventureiro-msn@hotmail.com>
2026-06-13 15:25:12 -0300

    Test mobile

--- api/tesouro.ts
+++ api/tesouro.ts
@@ -3,7 +3,7 @@ export default function handler(
   res: any
 ) {
   res.status(200).json({
-    titulo: "Tesouro Selic 2031",
+    titulo: "Tesouro Selic 2031, versão de Wesley Pereira Desenvolvedor Analista de Sistemas",
     taxa: 14.5,
     pu: 16782.34,
     atualizacao: "2026-06-13"
