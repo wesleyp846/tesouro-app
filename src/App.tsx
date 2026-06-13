4537f23f877cdc3d91bccfc49c95713686f1d2c2
wesleyp846 <aventureiro-msn@hotmail.com>
2026-06-13 15:14:39 -0300

    Consumindo API

--- src/App.tsx
+++ src/App.tsx
@@ -26,4 +26,37 @@ function App() {
   );
 }
 
+export default App;import { useEffect, useState } from "react";
+
+function App() {
+  const [mensagem, setMensagem] = useState("Carregando...");
+
+  useEffect(() => {
+    async function carregar() {
+      const response = await fetch("/api/tesouro");
+      const data = await response.json();
+
+      setMensagem(data.mensagem);
+    }
+
+    carregar();
+  }, []);
+
+  return (
+    <div
+      style={{
+        maxWidth: "600px",
+        margin: "40px auto",
+        padding: "20px",
+        border: "1px solid #ccc",
+        borderRadius: "8px",
+      }}
+    >
+      <h1>Tesouro Selic 2031</h1>
+
+      <p>{mensagem}</p>
+    </div>
+  );
+}
+
 export default App;
