3ff83af1966f50aa41b1c20cf1ea59d976c18615
wesleyp846 <aventureiro-msn@hotmail.com>
2026-06-13 15:54:28 -0300

    test by scraping

--- api/tesouro.ts
+++ api/tesouro.ts
@@ -1,9 +1,19 @@
+import * as cheerio from "cheerio";
+
 export default async function handler(
   req: any,
   res: any
 ) {
+  const response = await fetch(
+    "https://statusinvest.com.br/tesouro/tesouro-selic-2031"
+  );
+
+  const html = await response.text();
+
+  const $ = cheerio.load(html);
+
   res.status(200).json({
-    mensagem:
-      "Endpoint antigo do Tesouro foi removido (HTTP 410). Wesley Pereira Desenvolvedor Analista está trabalhando nisto"
+    tamanhoHtml: html.length,
+    titulo: $("h1").first().text(),
   });
 }
