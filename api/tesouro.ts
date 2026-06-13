67b8deb93747aea85d3b350ea2cb0b7abcd231dc
wesleyp846 <aventureiro-msn@hotmail.com>
2026-06-13 15:41:00 -0300

    api default

--- api/tesouro.ts
+++ api/tesouro.ts
@@ -2,20 +2,8 @@ export default async function handler(
   req: any,
   res: any
 ) {
-  try {
-    const response = await fetch(
-      "https://www.tesourodireto.com.br/json/br/com/b3/tesourodireto/service/api/treasurybondsinfo.json"
-    );
-
-    const text = await response.text();
-
-    res.status(200).json({
-      status: response.status,
-      primeiros500chars: text.substring(0, 500),
-    });
-  } catch (error: any) {
-    res.status(500).json({
-      erro: error.message,
-    });
-  }
+  res.status(200).json({
+    mensagem:
+      "Endpoint antigo do Tesouro foi removido (HTTP 410). Wesley Pereira Desenvolvedor Analista está trabalhando nisto"
+  });
 }
