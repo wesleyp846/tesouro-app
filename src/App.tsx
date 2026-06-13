2b6b5c54aae664e36bbbb0d5042cedf2f240150b
wesleyp846 <aventureiro-msn@hotmail.com>
2026-06-13 15:17:50 -0300

    Consumindo API

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,4 @@
-function App() {
-  return (
-    <div
-      style={{
-        maxWidth: "600px",
-        margin: "40px auto",
-        padding: "20px",
-        border: "1px solid #ccc",
-        borderRadius: "8px",
-      }}
-    >
-      <h1>Tesouro Selic 2031</h1>
-
-      <p>
-        <strong>Taxa:</strong> 14,50% a.a.
-      </p>
-
-      <p>
-        <strong>Preço Unitário:</strong> R$ 16.782,34
-      </p>
-
-      <p>
-        <strong>Atualização:</strong> 13/06/2026
-      </p>
-    </div>
-  );
-}
-
-export default App;import { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 
 function App() {
   const [mensagem, setMensagem] = useState("Carregando...");
